29de1b1a668b6870fad082f70f8afea1e2fb6595
ReiderJack <asetonlim@gmail.com>
2020-05-23 15:17:05 +0300

    start of server messages for sounds

---
+++ UnityProject/Assets/Scripts/ScriptableObject/Audio/PlaySoundMessageV2.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using Utility = UnityEngine.Networking.Utility;
+using Mirror;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace SO.Audio
+{
+	public class PlaySoundMessageV2 : ServerMessage
+	{
+		/*public int ComponentLocation;
+		public uint NetObject;
+		public Type ComponentType;
+
+		public static readonly Dictionary<ushort, Type> componentIDToComponentType = new Dictionary<ushort, Type>(); //These are useful
+		public static readonly Dictionary<Type, ushort> componentTypeToComponentID = new Dictionary<Type, ushort>();*/
+
+		/*static PlaySoundMessageV2()
+		{
+			//initialize id mappings
+			var alphabeticalComponentTypes =
+				typeof(SoundListEvent).Assembly.GetTypes()
+					.Where(type => typeof(SoundListEvent).IsAssignableFrom(type))
+					.OrderBy(type => type.FullName);
+			ushort i = 0;
+			foreach (var componentType in alphabeticalComponentTypes)
+			{
+				componentIDToComponentType.Add(i, componentType);
+				componentTypeToComponentID.Add(componentType, i);
+				i++;
+			}
+
+		}*/
+
+		public override void Process()
+		{
+			//LoadNetworkObject(NetObject);
+
+			//SoundSingleton.Instance.
+		}
+	}
+}
---
+++ UnityProject/Assets/Scripts/ScriptableObject/Audio/SoundSingleton.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace SO.Audio
+{
+	[CreateAssetMenu(fileName = "SoundSingleton", menuName = "Singleton/SoundSingleton")]
+	public class SoundSingleton : SingletonScriptableObject<SoundSingleton>
+	{
+		public List<SoundListEvent> uIActions = new List<SoundListEvent>();
+
+		public static Dictionary<ushort, SoundListEvent> IDtoSound = new Dictionary<ushort, SoundListEvent>();
+		public static Dictionary<SoundListEvent, ushort> SoundToID = new Dictionary<SoundListEvent, ushort>();
+
+		private static bool Initialised = false;
+
+		void OnEnable()
+		{
+			Setup();
+		}
+
+		void Setup()
+		{
+			ushort ID = 1;
+			var alphabeticaluIActions= uIActions.OrderBy(X => X.name);
+
+			foreach (var action in alphabeticaluIActions)
+			{
+				IDtoSound[ID] = action;
+				SoundToID[action] = ID;
+				ID++;
+			}
+			Initialised = true;
+		}
+
+		public SoundListEvent ReturnFromID(ushort ID)
+		{
+			if (!Initialised)
+			{
+				Setup();
+			}
+
+			if (IDtoSound.ContainsKey(ID))
+			{
+				return (IDtoSound[ID] as SoundListEvent);
+			}
+			return (null);
+		}
+
+		public void ActionCallServer(ushort ID, ConnectedPlayer SentByPlayer)
+		{
+			if (!Initialised)
+			{
+				Setup();
+			}
+			if (IDtoSound.ContainsKey(ID))
+			{
+				//IDtoSound[ID].SoundToID(SentByPlayer);
+			}
+		}
+
+	}
+}
