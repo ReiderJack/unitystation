5e6c23df19ce86bc7e0393bcd528b26ff19d1b5c
ReiderJack <asetonlim@gmail.com>
2020-06-02 17:53:37 +0300

    Beginning of set up but not working at all yet

--- UnityProject/Assets/Scripts/Managers/SoundManager.cs
+++ UnityProject/Assets/Scripts/Managers/SoundManager.cs
@@ -29,9 +29,6 @@ public class SoundManager : MonoBehaviour
 	private List<AudioSource> ambientTracks = new List<AudioSource>();
 	public AudioSource ambientTrack;
 
-	[SerializeField] private GameObject soundSpawnPrefab = null;
-	private List<SoundSpawn> pooledSources = new List<SoundSpawn>();
-
 	public static SoundManager Instance
 	{
 		get
@@ -120,97 +117,11 @@ public class SoundManager : MonoBehaviour
 		}
 	}
 
-	private void OnEnable()
-	{
-		SceneManager.activeSceneChanged += OnSceneChange;
-	}
-
-	private void OnDisable()
-	{
-		SceneManager.activeSceneChanged -= OnSceneChange;
-	}
-
-	void OnSceneChange(Scene oldScene, Scene newScene)
-	{
-		ReinitSoundPool();
-	}
-
-	void ReinitSoundPool()
-	{
-		for (int i = Instance.pooledSources.Count - 1; i > 0; i--)
-		{
-			if (Instance.pooledSources[i] != null)
-			{
-				Destroy(Instance.pooledSources[i].gameObject);
-			}
-		}
-
-		Instance.pooledSources.Clear();
-
-		// Cache some pooled sources:
-		for (int i = 0; i < 20; i++)
-		{
-			var soundObj = Instantiate(soundSpawnPrefab, transform);
-			pooledSources.Add(soundObj.GetComponent<SoundSpawn>());
-		}
-	}
-
 	/// <summary>
 	/// Uses a pooled AudioSource instead of the origianl one.
 	/// This copies the sourceToCopy settings to a source taken from the pool
 	/// and return it.
 	/// </summary>
-	private SoundSpawn GetSourceFromPool(AudioSource sourceToCopy)
-	{
-		for (int i = pooledSources.Count - 1; i > 0; i--)
-		{
-			if (pooledSources[i] != null && pooledSources[i].gameObject != null
-			                             && !pooledSources[i].isPlaying)
-			{
-				pooledSources[i].isPlaying = true;
-				CopySource(pooledSources[i].audioSource, sourceToCopy);
-				return pooledSources[i];
-			}
-		}
-
-		var soundObj = Instantiate(soundSpawnPrefab, transform);
-		var source = soundObj.GetComponent<SoundSpawn>();
-		pooledSources.Add(source);
-		source.isPlaying = true;
-		CopySource(source.audioSource, sourceToCopy);
-		return source;
-	}
-
-	private void CopySource(AudioSource newSource, AudioSource sourceToCopy)
-	{
-		newSource.clip = sourceToCopy.clip;
-		newSource.loop = sourceToCopy.loop;
-		newSource.pitch = sourceToCopy.pitch;
-		newSource.mute = sourceToCopy.mute;
-		newSource.spatialize = sourceToCopy.spatialize;
-		newSource.spread = sourceToCopy.spread;
-		newSource.volume = sourceToCopy.volume;
-		newSource.bypassEffects = sourceToCopy.bypassEffects;
-		newSource.dopplerLevel = sourceToCopy.dopplerLevel;
-		newSource.maxDistance = sourceToCopy.maxDistance;
-		newSource.minDistance = sourceToCopy.minDistance;
-		newSource.panStereo = sourceToCopy.panStereo;
-		newSource.rolloffMode = sourceToCopy.rolloffMode;
-		newSource.spatialBlend = sourceToCopy.spatialBlend;
-		newSource.bypassListenerEffects = sourceToCopy.bypassListenerEffects;
-		newSource.bypassReverbZones = sourceToCopy.bypassReverbZones;
-		newSource.reverbZoneMix = sourceToCopy.reverbZoneMix;
-		newSource.spatializePostEffects = sourceToCopy.spatializePostEffects;
-		newSource.outputAudioMixerGroup = sourceToCopy.outputAudioMixerGroup;
-		newSource.SetCustomCurve(AudioSourceCurveType.CustomRolloff,
-			sourceToCopy.GetCustomCurve(AudioSourceCurveType.CustomRolloff));
-		newSource.SetCustomCurve(AudioSourceCurveType.Spread,
-			sourceToCopy.GetCustomCurve(AudioSourceCurveType.Spread));
-		newSource.SetCustomCurve(AudioSourceCurveType.SpatialBlend,
-			sourceToCopy.GetCustomCurve(AudioSourceCurveType.SpatialBlend));
-		newSource.SetCustomCurve(AudioSourceCurveType.ReverbZoneMix,
-			sourceToCopy.GetCustomCurve(AudioSourceCurveType.ReverbZoneMix));
-	}
 
 	/// <summary>
 	/// Chooses a random sound matching the given pattern if the name contains a wildcard. (#)
@@ -313,7 +224,7 @@ public class SoundManager : MonoBehaviour
 		float pan = 0)
 	{
 		name = Instance.ResolveSoundPattern(name);
-		var sound = Instance.GetSourceFromPool(Instance.sounds[name]);
+		var sound = SoundSpawn.Instance.GetSourceFromPool(Instance.sounds[name]);
 		if (pitch > 0)
 		{
 			sound.audioSource.pitch = pitch;
@@ -344,7 +255,7 @@ public class SoundManager : MonoBehaviour
 	public static void Play(string name, bool polyphonic = false, bool Global = true)
 	{
 		name = Instance.ResolveSoundPattern(name);
-		Instance.PlaySource(Instance.GetSourceFromPool(Instance.sounds[name]));
+		Instance.PlaySource(SoundSpawn.Instance.GetSourceFromPool(Instance.sounds[name]));
 	}
 
 	private void PlaySource(SoundSpawn source, bool polyphonic = false, bool Global = true)
@@ -413,7 +324,7 @@ public class SoundManager : MonoBehaviour
 	{
 		name = Instance.ResolveSoundPattern(name);
 		if (!Instance.sounds.ContainsKey(name)) return;
-		var sound = Instance.GetSourceFromPool(Instance.sounds[name]);
+		var sound = SoundSpawn.Instance.GetSourceFromPool(Instance.sounds[name]);
 
 		if (pitch > 0)
 		{
@@ -450,34 +361,17 @@ public class SoundManager : MonoBehaviour
 	{
 		if (Instance.sounds.ContainsKey(name))
 		{
-			var sound = Instance.sounds[name];
+			var audioSource = Instance.sounds[name];
 
-			for (int i = Instance.pooledSources.Count - 1; i > 0; i--)
-			{
-				if (Instance.pooledSources[i] == null) continue;
-
-				if (Instance.pooledSources[i].isPlaying && Instance.pooledSources[i].audioSource.clip == sound.clip)
-				{
-					Instance.pooledSources[i].audioSource.Stop();
-				}
-			}
-
-			sound.Stop();
+			SoundSpawn.Instance.Stop(audioSource);
 		}
 		else
 		{
 			foreach (var sound in Instance.GetMatchingSounds(name))
 			{
-				var s = Instance.sounds[sound];
-				for (int i = Instance.pooledSources.Count - 1; i > 0; i--)
-				{
-					if (Instance.pooledSources[i] == null) continue;
-					if (Instance.pooledSources[i].isPlaying && Instance.pooledSources[i].audioSource.clip == s.clip)
-					{
-						Instance.pooledSources[i].audioSource.Stop();
-					}
-				}
-				s.Stop();
+				var audioSource = Instance.sounds[sound];
+				SoundSpawn.Instance.Stop(audioSource);
+				audioSource.Stop();
 			}
 		}
 	}
--- UnityProject/Assets/Scripts/Sound/SoundSpawn.cs
+++ UnityProject/Assets/Scripts/Sound/SoundSpawn.cs
@@ -1,13 +1,83 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SoundSpawn : MonoBehaviour
 {
+	private static SoundSpawn soundSpawn;
+
+	public static SoundSpawn Instance
+	{
+		get
+		{
+			if (soundSpawn == null)
+			{
+				soundSpawn = FindObjectOfType<SoundSpawn>();
+			}
+
+			return soundSpawn;
+		}
+	}
+
+	private List<SoundSpawn> pooledSources = new List<SoundSpawn>();
 	public AudioSource audioSource;
 	//We need to handle this manually to prevent multiple requests grabbing sound pool items in the same frame
 	public bool isPlaying = false;
 	private float waitLead = 0;
 	private bool monitor = false;
+
+	private void CopySource(AudioSource newSource, AudioSource sourceToCopy)
+	{
+		newSource.clip = sourceToCopy.clip;
+		newSource.loop = sourceToCopy.loop;
+		newSource.pitch = sourceToCopy.pitch;
+		newSource.mute = sourceToCopy.mute;
+		newSource.spatialize = sourceToCopy.spatialize;
+		newSource.spread = sourceToCopy.spread;
+		newSource.volume = sourceToCopy.volume;
+		newSource.bypassEffects = sourceToCopy.bypassEffects;
+		newSource.dopplerLevel = sourceToCopy.dopplerLevel;
+		newSource.maxDistance = sourceToCopy.maxDistance;
+		newSource.minDistance = sourceToCopy.minDistance;
+		newSource.panStereo = sourceToCopy.panStereo;
+		newSource.rolloffMode = sourceToCopy.rolloffMode;
+		newSource.spatialBlend = sourceToCopy.spatialBlend;
+		newSource.bypassListenerEffects = sourceToCopy.bypassListenerEffects;
+		newSource.bypassReverbZones = sourceToCopy.bypassReverbZones;
+		newSource.reverbZoneMix = sourceToCopy.reverbZoneMix;
+		newSource.spatializePostEffects = sourceToCopy.spatializePostEffects;
+		newSource.outputAudioMixerGroup = sourceToCopy.outputAudioMixerGroup;
+		newSource.SetCustomCurve(AudioSourceCurveType.CustomRolloff,
+			sourceToCopy.GetCustomCurve(AudioSourceCurveType.CustomRolloff));
+		newSource.SetCustomCurve(AudioSourceCurveType.Spread,
+			sourceToCopy.GetCustomCurve(AudioSourceCurveType.Spread));
+		newSource.SetCustomCurve(AudioSourceCurveType.SpatialBlend,
+			sourceToCopy.GetCustomCurve(AudioSourceCurveType.SpatialBlend));
+		newSource.SetCustomCurve(AudioSourceCurveType.ReverbZoneMix,
+			sourceToCopy.GetCustomCurve(AudioSourceCurveType.ReverbZoneMix));
+	}
+
+	public SoundSpawn GetSourceFromPool(AudioSource sourceToCopy)
+	{
+		for (int i = pooledSources.Count - 1; i > 0; i--)
+		{
+			if (pooledSources[i] != null
+			    && pooledSources[i].gameObject != null
+			    && !pooledSources[i].isPlaying)
+			{
+				pooledSources[i].isPlaying = true;
+				CopySource(pooledSources[i].audioSource, sourceToCopy);
+				return pooledSources[i];
+			}
+		}
+
+		var source = GetComponent<SoundSpawn>();
+		pooledSources.Add(source);
+		source.isPlaying = true;
+		CopySource(source.audioSource, sourceToCopy);
+		return source;
+	}
 
 	public void PlayOneShot()
 	{
@@ -29,14 +99,61 @@ public class SoundSpawn : MonoBehaviour
 		monitor = true;
 	}
 
+	public void Stop(AudioSource audioSource)
+	{
+		for (int i = Instance.pooledSources.Count - 1; i > 0; i--)
+		{
+			if (Instance.pooledSources[i] == null) continue;
+
+			if (Instance.pooledSources[i].isPlaying
+			    && Instance.pooledSources[i].audioSource.clip == audioSource.clip)
+			{
+				Instance.pooledSources[i].audioSource.Stop();
+			}
+		}
+		audioSource.Stop();
+	}
+
 	private void OnEnable()
 	{
+		// Cache some pooled sources:
+		for (int i = 0; i < 20; i++)
+		{
+			pooledSources.Add(this);
+		}
+		//ReinitSoundPool();
+		//SceneManager.activeSceneChanged += OnSceneChange;
 		UpdateManager.Add(CallbackType.UPDATE, UpdateMe);
 	}
 
 	private void OnDisable()
 	{
+		//SceneManager.activeSceneChanged -= OnSceneChange;
 		UpdateManager.Remove(CallbackType.UPDATE, UpdateMe);
+	}
+
+	void OnSceneChange(Scene oldScene, Scene newScene)
+	{
+		ReinitSoundPool();
+	}
+
+	void ReinitSoundPool()
+	{
+		for (int i = Instance.pooledSources.Count - 1; i > 0; i--)
+		{
+			if (Instance.pooledSources[i] != null)
+			{
+				Destroy(Instance.pooledSources[i].gameObject);
+			}
+		}
+
+		Instance.pooledSources.Clear();
+
+		// Cache some pooled sources:
+		for (int i = 0; i < 20; i++)
+		{
+			pooledSources.Add(this);
+		}
 	}
 
 	void UpdateMe()
