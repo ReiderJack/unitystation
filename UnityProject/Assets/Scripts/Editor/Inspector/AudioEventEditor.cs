cbd9cfb3a0b6aa38727b09f17fd8a0415dadbaed
ReiderJack <asetonlim@gmail.com>
2020-06-23 22:27:01 +0300

    AudioEvent custom editor

---
+++ UnityProject/Assets/Scripts/Editor/Inspector/AudioEventEditor.cs
@@ -0,0 +1,39 @@
+﻿using Audio.Containers;
+using UnityEditor;
+using UnityEngine;
+
+namespace CustomInspectors
+{
+	[CustomEditor(typeof(AudioEvent), true)]
+	public class AudioEventEditor : Editor
+	{
+		private AudioSource audioSource;
+
+		private void OnEnable()
+		{
+			audioSource = EditorUtility.CreateGameObjectWithHideFlags("Audio preview",HideFlags.HideAndDontSave,typeof(AudioSource)).GetComponent<AudioSource>();
+		}
+
+		private void OnDisable()
+		{
+			DestroyImmediate(audioSource.gameObject);
+		}
+
+		public override void OnInspectorGUI()
+		{
+			DrawDefaultInspector();
+
+			EditorGUI.BeginDisabledGroup(serializedObject.isEditingMultipleObjects);
+			if (GUILayout.Button("Play"))
+			{
+				((AudioEvent)target).Play(audioSource);
+			}
+
+			if (GUILayout.Button("Stop"))
+			{
+				audioSource.Stop();
+			}
+			EditorGUI.EndDisabledGroup();
+		}
+	}
+}
