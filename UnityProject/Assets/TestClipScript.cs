3cceb23711f0918275d324909ac201e984ca6122
ReiderJack <asetonlim@gmail.com>
2020-06-23 15:57:09 +0300

    Files for testing

---
+++ UnityProject/Assets/TestClipScript.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Audio;
+using Audio.Containers;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TestClipScript : MonoBehaviour, ICheckedInteractable<HandApply>
+{
+	public MyTestEvent testEvent;
+	public bool WillInteract(HandApply interaction, NetworkSide side)
+	{
+		//start with the default HandApply WillInteract logic.
+		if (!DefaultWillInteract.Default(interaction, side)) return false;
+
+		return true;
+	}
+
+	public void ServerPerformInteraction(HandApply interaction)
+	{
+		//AudioSourceData.PlayServer(gameObject);
+		testEvent?.Invoke(gameObject);
+	}
+}
+[System.Serializable]
+public class MyTestEvent : UnityEvent<GameObject> { }
